07f958be6c92fe96bd44eef6272bc6e6f90abd5f
Fireye04 <codekai16@gmail.com>
2024-06-22 23:26:52 -0700

    Functional dialogue manager!!!

--- Scripts/Lyra.cs
+++ Scripts/Lyra.cs
@@ -3,16 +3,23 @@ using System;
 
 public partial class Lyra : Node, IDialogueSource {
 
+    private int convoCount = 0;
 
-	private NPC_Resource npc_Resource;
+    private NPC_Resource npc_Resource;
 
-	public override void _Ready() {
-		npc_Resource = GD.Load<NPC_Resource>("res://Resources/Lyra_Silverwind.tres");
+    public override void _Ready() {
+        npc_Resource = GD.Load<NPC_Resource>("res://Resources/Lyra_Silverwind.tres");
 
-	}
+    }
 
-	public string getConversation() {
-		return "start";
-	}
+    public string getConversation() {
+        convoCount += 1;
+        if (convoCount == 1) {
+            return "convo1";
+        } else {
+            return "catchall";
+        }
+
+    }
 
 }
