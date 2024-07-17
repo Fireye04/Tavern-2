db8a77cf86556717b65fd19b05ce411c5bcc7544
Fireye04 <codekai16@gmail.com>
2024-07-17 15:01:00 -0700

    Non-table NPC Spawner

---
+++ Scripts/objects/NPCSpawner.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public partial class NPCSpawner : Node2D {
+
+	[Export]
+	public NPC_Resource npcr;
+
+	[Export]
+	public barter bItem { get; set; }
+
+	[Export]
+	public PackedScene pnpc;
+
+	public NPC npc;
+
+	public override void _Ready() {
+		spawnNpc();
+	}
+
+	public void spawnNpc() {
+		GD.Print("spawning " + npcr.Name);
+		npc = (NPC)pnpc.Instantiate();
+		AddChild(npc);
+		npc.init(npcr, bItem);
+		npc.Position = Position;
+
+
+	}
+}
